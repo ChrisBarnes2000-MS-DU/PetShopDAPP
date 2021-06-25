9bd20b1c59560dbf01b6f836d57b04763451e906
Christopher Barnes <chris.barnes.2000@me.com>
2021-06-25 03:02:56 -0700

    [Add]: Adoption function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,5 +1,14 @@
 pragma solidity >=0.5.0;
 
 contract Adoption {
-    
+    address[16] public adopters;
+
+    // Adopting a pet
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
 }
