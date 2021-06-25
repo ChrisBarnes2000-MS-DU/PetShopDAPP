3c9d63b084b6b53e8056819d34204d1745809c9c
Christopher Barnes <chris.barnes.2000@me.com>
2021-06-25 03:22:29 -0700

    [Add]: Contract Init

--- src/js/app.js
+++ src/js/app.js
@@ -49,9 +49,17 @@ App = {
   },
 
   initContract: function () {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function (data) {
+      // Get the necessary contract artifact file and instantiate it with @truffle/contract
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      // Set the provider for our contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
